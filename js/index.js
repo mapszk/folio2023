61eea61aa3659c885875b6fe686749df95146836
Martin Pilaszek <martinpilaszek@gmail.com>
2023-08-21 19:34:09 -0300

    Added logo animatin

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var animation = window.bodymovin.loadAnimation({
+  container: document.getElementById('logo-animation'),
+  path: '../assets/ojo.json',
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  name: "Logo",
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+
+})
